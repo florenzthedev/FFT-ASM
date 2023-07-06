78fd2fe22abe679ba4a55426182932899183a985
Florenz <florenzthedev@gmail.com>
2023-07-05 23:26:01 -0700

    First working version

--- demo/demo.c
+++ demo/demo.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "fft_asm.h"
 
 #define MAX_SAMPLES 127
+
+unsigned int bit_length(unsigned int x) {
+  unsigned int bits = 0;
+  while (x) {
+    bits++;
+    x >>= 1;
+  }
+  return bits;
+}
+
+unsigned int bit_reverse(unsigned int x, unsigned int n) {
+  unsigned int r = 0;
+  for (unsigned int i = 0; i < n; ++i) {
+    if (x & (1 << i)) r |= 1 << ((n - 1) - i);
+  }
+  return r;
+}
+
+void bit_reversal_permutation(double complex *x, int N) {
+  assert((N & (N - 1)) == 0);  // Must be a power of two
+
+  // Don't forget bit_length is one indexed!
+  int bl = bit_length(N) - 1;
+
+  // We can skip the first and last index, they never need to be moved
+  for (int i = 1; i < N - 1; i++) {
+    int ri = bit_reverse(i, bl);
+    if (i < ri) {
+      double complex temp = x[i];
+      x[i] = x[ri];
+      x[ri] = temp;
+    }
+  }
+}
 
 int main(int argc, char *argv[]){
   if(argc != 2){
@@ -25,10 +60,11 @@ int main(int argc, char *argv[]){
     }
   fclose(fp);
 
-  
   double complex X[N];
   for(int j = 0; j < N; j++)
   	X[j] = x[j];
+  bit_reversal_permutation(X,N);
+
   fft(X,N);
 
   printf("x:\n");
--- fft.s
+++ fft.s
@@ -1,6 +1,10 @@
 .data
 TAU:
-  .double 6.28318530717958647692
+  .double -6.28318530717958647692
+X:
+  .quad 0
+N:
+  .quad 0
 exponent:
   .double 0
 temp:
@@ -17,29 +21,101 @@ fft:
   push  %r14
   push  %r15
   # Register usage:
+  #   r11 - temporary, usually the offset between the even/odd pairings 
   #   r12 - current block size, starts with 2 and doubles each pass until size of N
-  #   r13 - start of current block, starts at 0 and has r8 added each pass until size N
-  #   r14 - current element pair in block, starts at 0 and counts to r8/2
+  #   r13 - current element pair in block, starts at 0 and counts to r12/2
+  #   r14 - start of current block, starts at 0 and has r12 added each pass until size N
   #   r15 - index of start of current element pair
   #   xmm0  - the real part of omega for the current iteration
   #   xmm1  - the imaginary part of omega for current iteration
-  movq  $2, %r12
-  movq  $0, %r13
-  movq  $0, %r14
-  
+  #   xmm2  - temporary, usually the last loaded odd real portion from the array
+  #   xmm3  - temporary, usually the last loaded odd imaginary portion from the array
+  #   xmm4  - temporary, usually scratch space for complex multiplication
+  #   xmm5  - temporary, usually scratch space for complex multiplication
+  #   xmm6  - temporary, usually the last loaded even real portion from the array
+  #   xmm7  - temporary, usually the last loaded even imaginary portion from the array  
+  movq  %rdi,   X(%rip)
+  movq  %rsi,   N(%rip)
+  movq  $2,     %r12
+
+block_loop:
+  xorl  %r13d,  %r13d # Its how Intel and AMD recommend setting a register to 0
+
+element_loop:
+  xorl  %r14d,  %r14d
+
   # Calculate omega
   movsd TAU(%rip),  %xmm0
-  cvtsi2sdq %r10,   %xmm1
+  cvtsi2sdq %r13,   %xmm1
   mulsd     %xmm1,  %xmm0
-  cvtsi2sdq %r8,    %xmm1
+  cvtsi2sdq %r12,    %xmm1
   divsd     %xmm1,  %xmm0
   movsd     %xmm0,  exponent(%rip)
-  call cos
+  call sin
   movsd %xmm0,          temp(%rip)
   movsd exponent(%rip), %xmm0
-  call sin
+  call cos
   movsd temp(%rip), %xmm1
 
+  # Reload N from memory so we don't have to again later
+  movq  N(%rip), %rsi
+
+pair_loop:
+  # Load next even entry from array 
+  xorl  %r15d,          %r15d
+  addq  %r13,           %r15
+  addq  %r14,           %r15
+  shl   $1,             %r15
+  movq  X(%rip),        %rdi
+  movsd (%rdi,%r15,8),  %xmm6 # real portion, the C99 standard does gaurentee this ordering
+  movsd 8(%rdi,%r15,8), %xmm7 # imaginary portion
+
+  # Load next odd entry from array
+  addq  %r12,   %r15            # no need to /2 because we are working with complex addresses
+  movsd (%rdi,%r15,8),  %xmm2
+  movsd 8(%rdi,%r15,8), %xmm3
+
+  # Multiply odd element by omega, (a + b)(c + d)
+  movsd %xmm3,  %xmm4
+  mulsd %xmm0,  %xmm4 # ad
+  movsd %xmm1,  %xmm5
+  mulsd %xmm2,  %xmm5 # bc
+  addsd %xmm5,  %xmm4 # ad + bc
+  mulsd %xmm0,  %xmm2 # ac
+  mulsd %xmm1,  %xmm3 # bd              
+  subsd %xmm3,  %xmm2 # ac + bd (i^2)
+  movsd %xmm4,  %xmm3
+  # omega * odd now in xmm2 and xmm3 (i)
+
+  # even - omega * odd
+  movsd %xmm6,  %xmm4
+  movsd %xmm7,  %xmm5
+  subsd %xmm2,  %xmm4
+  subsd %xmm3,  %xmm5
+  movsd %xmm4,  (%rdi,%r15,8)  
+  movsd %xmm5,  8(%rdi,%r15,8) 
+
+  # even + omega * odd
+  addsd %xmm2,  %xmm6
+  addsd %xmm3,  %xmm7
+  subq  %r12,   %r15
+  movsd %xmm6,  (%rdi,%r15,8)  
+  movsd %xmm7,  8(%rdi,%r15,8) 
+
+  addq  %r12, %r14
+  cmpq  %rsi, %r14
+  jl    pair_loop
+
+  inc   %r13
+  movq  %r13, %rax
+  shl   $1,   %rax
+  cmpq  %r12, %rax
+  jl    element_loop
+
+  shl   $1, %r12
+  cmpq  %rsi, %r12
+  jle   block_loop
+  
   pop %r15
   pop %r14
   pop %r13
